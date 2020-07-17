94fee685fe3d268c7d6a84e25508b37639203cd5
hechengjin <he_chengjin@outlook.com>
2020-07-17 15:36:11 +0800

    安装后的变动

---
+++ upload/config/config_ucenter.php
@@ -0,0 +1,19 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'chunhuixuefu.com');
+define('UC_DBNAME', 'chdb');
+define('UC_DBCHARSET', 'gbk');
+define('UC_DBTABLEPRE', '`chdb`.firemail_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'gbk');
+define('UC_KEY', 'E2Y809W30dnaxbgfJ5c7B30co4icC9z1161fu0X7918bWef2x4y2Lbh5w8O2U5Xb');
+define('UC_API', 'http://www.firemail.wang/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
